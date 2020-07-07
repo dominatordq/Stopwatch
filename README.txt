b2bb3d0c67bb05605b59ed32883a6504aa88b44f
dominatordq <35786422+dominatordq@users.noreply.github.com>
2020-07-07 16:13:18 -0600

    Uploaded .js and .html files. Included README

---
+++ README.txt
@@ -0,0 +1,6 @@
+README
+
+For easiest compilation, run Live Server with the stopwatch.html file in VSCode or another platform.
+
+In the console window, type sw.reset() to reset stopwatch, sw.start() to start it, and sw.stop() to pause it.
+
---
+++ stopwatch.js
@@ -0,0 +1,41 @@
+/*
+ * Author: Dominic Quintana
+ * Fun little stopwatch script!
+*/
+
+function Stopwatch() {
+    this.duration = 0;
+    let currentTime;
+    let stopTime;
+
+    let status = { start: false };
+    // resets duration and status of stopwatch
+    this.reset = function() {
+        this.duration = 0;
+        status.start = false;
+    };
+    // starts the stopwatch
+    this.start = function() {
+        // check to see if it is already started first
+        if (status.start === true)
+            throw new Error('Stopwatch is already started.');
+        
+        currentTime = Date.now(); // get time of start
+        status['start'] = true;
+    }
+    // pauses the stopwatch
+    this.stop = function() {
+        // check to see if it is already stopped
+        if (status.start === false)
+            throw new Error('Stopwatch is already stopped.');
+
+        stopTime = Date.now();
+        this.duration = ((stopTime - currentTime) / 1000) + this.duration;
+        console.log(this.duration);
+        status['start'] = false;
+    }
+}
+
+const sw = new Stopwatch();
+sw.reset();
+console.log(sw.duration);
